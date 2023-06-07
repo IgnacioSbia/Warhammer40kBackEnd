0594758400edc8c3e733addf51385efe732b1400
Ignacio <ignaciosabiajes@gmail.com>
2023-06-07 11:09:59 -0300

    Upload user pfp 20%

--- Routes/w40kroutes.js
+++ Routes/w40kroutes.js
@@ -2,18 +2,24 @@ const express = require("express");
 const routes = express.Router();
 const {verifyToken} = require('../Middlewares/authUser');
 const { getRaces, regUser, loginTo, getUserMail, insertUserFavourite, deleteFavouriteRace } = require("../Controllers/w40kcontrollers");
+//const { default: upload } = require("../Middlewares/multer");
+
 
 //getRaces
 routes.get('/races', getRaces);
 //Register
 routes.post('/register', regUser);
 //Login
-routes.post('/login', loginTo)
+routes.post('/login', loginTo);
 //GetUserNameById
-routes.get('/userEmail',getUserMail)
+routes.get('/userEmail',getUserMail);
 //Insert Favourite Race to User
-routes.post('/FavRace' ,verifyToken,insertUserFavourite)
+routes.post('/FavRace' ,verifyToken,insertUserFavourite);
 //Delete Favourite Race to User
-routes.delete('/deleteFavRace',verifyToken,deleteFavouriteRace)
+routes.delete('/deleteFavRace',verifyToken,deleteFavouriteRace);
+//upload User Image
+//routes.post("/uploadImg", upload.single('image'))
+
+
 //export
 module.exports = routes;
